66a2c6e3d79396e26268e1f40bd147a232342bc8
kamikaze823 <christopher.lo2008@gmail.com>
2012-10-11 18:59:04 -0700

    start of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futurescientist::Application.config.secret_token = '67c0cf4e574aaaa25bc3cc07478f1d19a3a41f90aa6ee7a06c7811e4f11dc713543d87ab9000102ab0fc2c00b027cec7ada283ddd2fa2e540037856ec3d337fb'
