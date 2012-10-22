9c3fff6d7cccc12d27778887b3d6cbf178d1013f
kamikaze823 <christopher.lo2008@gmail.com>
2012-10-21 22:16:50 -0700

    fixed database/got show problems to work

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -3,4 +3,23 @@ class ProblemsController < ApplicationController
   def index
 end
 
+  def show
+    id = params[:id] 
+    @problem = Problem.find(id)
+    puts @problem
+    puts @problem.summary
+    user_id = @problem.user_id
+    puts user_id
+    @user = User.find(user_id)
+    puts @user
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
 end
