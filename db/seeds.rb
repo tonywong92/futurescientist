2ed9426e396d5245f2c9a9b33a69ed0c43ffad35
Tony Wong <tonywong@berkeley.edu>
2012-10-21 23:42:04 -0700

    working on cucumber

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Skill.create!(:skill_name => 'water')
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /^the create account page$/
+      '/accounts/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
