b745c6377efc44f7a55e82744840b6ad0d363915
Tony Wong <tonywong@berkeley.edu>
2012-10-21 23:55:49 -0700

    continuing cucumber tests

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,7 +4,7 @@ class Account < ActiveRecord::Base
   belongs_to :user
   has_many :problems, :through => :users
 
-  validates :account_name, :presence => true
-  validates :password, :presence => true
+  #validates :account_name, :presence => true
+  #validates :password, :presence => true
 
 end
