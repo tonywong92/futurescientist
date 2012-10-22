930982f14b7aa3b85287fcb4ebe6b543e69d17f3
Tony Wong <tonywong@berkeley.edu>
2012-10-21 21:06:09 -0700

    continued working on account creation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,10 @@
 class Account < ActiveRecord::Base
-  attr_accessible :admin, :email, :name, :password, :skills, :verified_skills
+  attr_accessible :admin, :email, :account_name, :password, :skills, :verified_skills
   
   belongs_to :user
   has_many :problems, :through => :users
 
-  validates :name, :presence => true
-  validates :password, :presence => true
+  validates :account_name, :presence => true
+  validates :password, :presence => true  
 
 end
