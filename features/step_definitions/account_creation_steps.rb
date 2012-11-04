e3c2bbf5ef347ac4a67cb9f8471ed05bddc23454
saasbook <saasbook@saasbook.(none)>
2012-11-04 00:47:21 -0700

    working on admin acct

--- features/step_definitions/account_creation_steps.rb
+++ features/step_definitions/account_creation_steps.rb
@@ -2,6 +2,26 @@ Given /^I add the "(.*)" skill to the database$/ do |skill|
   Skill.create!(:skill_name => skill)
 end
 
+Given /^the site is set up$/ do
+  step "I am on the home page"
+  step "I go to the create account page"
+  step "I fill in \"account_name\" with \"admin\""
+  step "I fill in \"password\" with \"password\""
+  step "I fill in \"name\" with \"Admin\""
+  step "I fill in \"phone_number\" with \"1234567890\""
+  step "I check \"Admin\""
+  step "I press \"Create Account\""
+  step "I should be on the problems page"
+end
+
+When /^I am logged in as an admin$/
+  step "I go to the login page"
+  step "I fill in \"account_account_name\" with \"admin\""
+  step "I fill in \"account_password\" with \"password\""
+  step "I press \"Login\""
+  step "I should see \"Welcome, Admin\""
+end
+
 When /^I fill in the following fields:$/ do |table|
   table.hashes.each do |account|
     step "I fill in \"email\" with \"#{account['Email']}\""
