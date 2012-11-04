fcf1cb69838b765e3aa23f9d6f94f0f6b006c98d
Tony Wong <tonywong@berkeley.edu>
2012-11-03 23:13:43 -0700

    started working on sms_create in problems controller

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -41,16 +41,31 @@ class ProblemsController < ApplicationController
   end
 
   def create
-	@problem = Problem.new(:location => params[:user][:location], :summary => params[:problem][:summary], :description => params[:problem][:description], :skills => params[:skills])
+    @problem = Problem.new(:location => params[:user][:location], :summary => params[:problem][:summary], :description => params[:problem][:description], :skills => params[:skills])
 
-	@user = User.find_by_phone_number(params[:user][:phone_number])
-	if @user.nil?
-	@user = User.new
-	@user.attributes = params[:user]
-	end
-	@user.problems << @problem
-	save_problem
-	return
+    @user = User.find_by_phone_number(params[:user][:phone_number])
+    if @user.nil?
+      @user = User.new
+      @user.attributes = params[:user]
+    end
+    @user.problems << @problem
+    save_problem
+    return
+  end
+  
+  def sms_create
+    success_msg = "You have successfully posted your problem. We will notify you of any updates as soon as possible. Thank you for using Emplify!"
+    failure_msg = "Sorry, something seems to have gone wrong. We can't post your problem at this time."
+    
+    problem_text = params[:Body]
+    twiml = Twilio::TwiML::Response.new do |r|
+      if success
+        r.Sms success_msg
+      else
+        r.Sms failure_msg
+      end
+    end
+    twiml.text
   end
 
   def new
