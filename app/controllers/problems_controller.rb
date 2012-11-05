ac47fc281970f209a508617fa17348eb3d19dd63
James Won <james.won@berkeley.edu>
2012-11-04 22:30:06 -0800

    fixed stupid duplicate problem controller thing

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,142 +1,6 @@
 class ProblemsController < ApplicationController
 
   def index
-    @problems = Problem.find(
-	    	  :all,
-	      	  :order => "created_at DESC")
-  end
-
-  def create
-    @problem = Problem.new(:location => params[:problem][:location], :summary => params[:problem][:summary], :description => params[:problem][:description], :skills => params[:skills])
-
-    @user = User.find_by_phone_number(params[:user][:phone_number])
-    if @user.nil?
-      @user = User.new
-      @user.attributes = params[:user]
-    end
-    @user.problems << @problem
-    save_problem
-    return
-  end
-  
-  #sms superfunction for receiving texts
-  def receive_sms
-=begin
-    problem_text = params[:Body].split
-    case problem_text[0]
-      when /^ADD$/
-        sms_create problem_text
-    end
-=end
-
-    account_sid = 'AC65e34f3e42326c21b8d1c915c1817f7e'
-    auth_token = '0814d38b55c49cfc462463d643328287'
-    client = Twilio::REST::Client.new account_sid, auth_token
-
-    client.account.sms.messages.create(:from => "+16502674928", :to => "+14154393733", :body => "TESTING")
-  end
-  
-  # sms support for problem creation
-  def sms_create problem_text
-    success_msg = "You have successfully posted your problem. We will notify you of any updates as soon as possible. Thank you for using Emplify!"
-    failure_msg = "Sorry, something seems to have gone wrong. We can't post your problem at this time."
-    summary = problem_text[1]
-    location = problem_text[2]
-    skills = problem_text[3]
-    
-    @problem = Problem.new(:location => location, :summary => summary, :skills => skills)
-    add_problem_to_user_sms
-    @account_sid = 'AC65e34f3e42326c21b8d1c915c1817f7e'
-    @auth_token = '0814d38b55c49cfc462463d643328287'
-    
-    if save_problem_sms
-      body = success_msg
-    else
-      body = failure_msg
-    end
-
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-    
-    @account = @client.account
-    @message = @account.sms.messages.create({:from => '+16502674928', :to => '+14154393733', :body => body})
-    puts @message
-    if false
-    twiml = Twilio::TwiML::Response.new do |r|
-      if save_problem_sms
-        r.Sms success_msg
-      else
-        r.Sms failure_msg
-      end
-    end
-    twiml.text
-    end
-  end
-  
-  def add_problem_to_user_sms
-    @user = User.find_by_phone_number(params[:From])
-    if @user.nil?
-      @user = User.new(:phone_number => params[:From])
-    end
-    @user.problems << @problem
-  end
-
-  def new
-    @all_skills = Skill.find(:all)
-    render 'problems/new'
-  end
-
-  def save_problem
-    if @user.save
-      flash[:notice] = 'You have successfully created a problem!'
-    else
-      flash[:error] = 'There was a problem with creating the problem.'
-    end
-    redirect_to problems_path
-  end
-  
-  def save_problem_sms
-    if @user.save
-      return true
-    else
-      return false
-    end
-  end
-
-  def show
-    id = params[:id] 
-    @problem = Problem.find(id)
-    @user = @problem.user
-  end
-
-  def edit
-   #TODO: NEED TO ADD LOGIC about how to verify person editing.
-    @problem = Problem.find(params[:id])
-    @all_skills = Skill.find(:all)
-    @user = @problem.user
-  end
-
-  def update
-    #TODO: NEED TO ADD LOGIC FOR updating user.
-    @problem = Problem.find(params[:id])
-    @problem.update_attributes!(params[:problem])
-    flash[:notice] = "#{@problem.summary} was successfully updated."
-    redirect_to problem_path(@problem)
-  end
-
-  def destroy
-    #TODO: NEED TO ADD LOGIC about how to verify person editing.
-    @problem = Problem.find(params[:id])
-    @problem.destroy
-    flash[:notice] = "Problem '#{@problem.summary}' deleted."
-    redirect_to problems_path
-  end
-
-end
-
-class ProblemsController < ApplicationController
-
-  def index
     @sessionSkills = []
     @sessionAddresses = []
     skills = params[:skills] || session[:skills] || {}
@@ -191,11 +55,18 @@ class ProblemsController < ApplicationController
 
   #sms superfunction for receiving texts
   def receive_sms
+=begin
     problem_text = params[:Body].split
     case problem_text[0]
       when /^ADD$/
         sms_create problem_text
     end
+=end
+    account_sid = 'AC65e34f3e42326c21b8d1c915c1817f7e'
+    auth_token = '0814d38b55c49cfc462463d643328287'
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    client.account.sms.messages.create(:from => "+16502674928", :to => "+14154393733", :body => "TESTING")
   end
 
   # sms support for problem creation
