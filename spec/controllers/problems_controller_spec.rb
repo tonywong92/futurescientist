173bad46e8fe6a3ee3270eb3f967a25c15182a1f
Tony Wong <tonywong@berkeley.edu>
2012-11-11 23:19:03 -0800

    added rspec tests for provider acceptance of problem

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -6,20 +6,41 @@ describe ProblemsController do
     clear_messages
   end
   
-  describe 'problem submission through text' do
+  describe 'sms interactions' do
     let(:twilio_phone_number) {"+16502674928"}
     let(:registered_phone_number) {"+14154393733"}
+    
+    describe 'problem submission through text' do
+      before do
+        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ADD textedProblem Location Skill'}
+      end
+    
+      it 'should post my problem' do
+        textedProblem = Problem.find_by_summary('textedProblem')
+        textedProblem.should_not be_nil
+        textedProblem.location.should == "Location"
+        textedProblem.skills.should == "Skill"
+      end
+    end
   
-    before do
-      post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ADD textedProblem Location Skill'}
+    describe 'requester accepting a problem through text' do
+      before do
+        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ADD textedProblem Location Skill'}
+        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ACCEPT textedProblem 1'}
+      end
+      
+      it 'should remove the specified problem from the index and assign it to the provider account' do
+        problem = Problem.find(1)
+        problem.accepted.should == true
+        provider = User.find_by_phone_number(registered_phone_number).account
+        provider.accepted_problems.include?(problem).should == true
+      end
+      
+      it 'should send a confirmation text to the provider' do
+        open_last_text_message_for "14154393733"
+        current_text_message.should have_body "You have successfully accepted this problem! Please contact the requester as soon as possible"
+      end
     end
-    
-    it 'should post my problem' do
-      textedProblem = Problem.find_by_summary('textedProblem')
-      textedProblem.should_not be_nil
-      textedProblem.location.should == "Location"
-      textedProblem.skills.should == "Skill"
-    end
-    
+  
   end
 end
