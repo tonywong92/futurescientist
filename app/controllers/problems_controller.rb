13e1e612ab572922e68fbe6be214a899119fac86
Tony Wong <tonywong@berkeley.edu>
2012-11-16 22:43:26 -0800

    fixed bug in sms_accept_problem

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -260,7 +260,7 @@ class ProblemsController < ApplicationController
     problem_id = @problem_text[1]
     password = @problem_text[2]
     provider_user = User.find_by_phone_number(normalize_phone(params[:From]))
-    @provider_acc = provider_user.account
+    provider_acc = provider_user.account
     if provider_acc.nil?
       body = "Sorry, there is no account associated with your phone number"
     else #if provider_acc.password == password
