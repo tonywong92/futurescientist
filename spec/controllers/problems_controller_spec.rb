fd04d61332dbb61db4cb2f4b0d2c0ee2208ea182
Tony Wong <tonywong@berkeley.edu>
2012-11-18 00:04:36 -0800

    in the process of fixing rspec tests

--- spec/controllers/problems_controller_spec.rb
+++ spec/controllers/problems_controller_spec.rb
@@ -9,38 +9,45 @@ describe ProblemsController do
 
   describe 'sms interactions' do
     let(:twilio_phone_number) {"+16502674928"}
-    let(:registered_phone_number) {"+14154393733"}
-    let(:registered_phone_number2) {"+14154393734"}
+    let(:registered_phone_number) {"+16263204917"}
+    let(:registered_phone_number2) {"+14154393733"}
 
     describe 'problem submission through text' do
       before do
-        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ADD textedProblem Location Skill'}
+        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'Add #texted problem @San Francisco !water electrical'}
       end
 
       it 'should post my problem' do
-        textedProblem = Problem.find_by_summary('textedProblem')
+        textedProblem = Problem.find_by_summary('texted problem')
         textedProblem.should_not be_nil
-        textedProblem.location.should == "Location"
-        textedProblem.skills.should == "Skill"
+        textedProblem.location.should == "San Francisco"
+        textedProblem.skills.should == "water electrical"
       end
     end
 
     describe 'requester accepting a problem through text' do
       before do
-        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ADD textedProblem Location Skill'}
-        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'ACCEPT textedProblem 1'}
+        provider_account = Account.create(:account_name => 'tony', :password => 'Password')
+        provider_user = User.create(:phone_number => registered_phone_number2)
+        provider_user.account = provider_account
+        provider_user.save
+        User.stub(:find_by_phone_number).and_return(provider_user)
+        requester = User.create(:phone_number => registered_phone_number)
+        post :receive_sms, {:From => registered_phone_number, :To => twilio_phone_number, :Body => 'Add #textedProblem2 @Location !water'}
+        post :receive_sms, {:From => registered_phone_number2, :To => twilio_phone_number, :Body => 'Accept textedProblem2 1'}
       end
 
-      it 'should remove the specified problem from the index and assign it to the provider account' do
+      it 'should mark the problem as done' do
         problem = Problem.find(1)
-        problem.accepted.should == true
-        provider = User.find_by_phone_number(registered_phone_number).account
-        provider.accepted_problems.include?(problem).should == true
+        problem.summary.should == "textedProblem2"
+        problem.archived.should == true
       end
 
-      it 'should send a confirmation text to the provider' do
+      it 'should send a confirmation text to the provider and notify the requester' do
         open_last_text_message_for "14154393733"
-        current_text_message.should have_body "You have successfully accepted this problem! Please contact the requester as soon as possible"
+        current_text_message.should have_body "You have accepted problem #1. Please contact your provider at +#{registered_phone_number2} as soon as possible."
+        open_last_text_message_for "16263204917"
+        current_text_message.should have_body "Your textedProblem2 problem has been accepted by tony, whom you can contact at +14154393733"
       end
     end
 
@@ -53,7 +60,7 @@ describe ProblemsController do
       end
 
       it 'should send me a list of problems with correct attributes' do
-        open_last_test_message_for "+14154393734"
+        open_last_text_message_for "+14154393734"
         current_text_message.should have_body "#{Problem.find_by_summary(Problem1)}. @Location1 !water #Problem1 $50 #{Problem.find_by_summary(Problem2)}. @Location2 !water #Problem2 $50 "
       end
     end
