d9137d3296ceb8b34a1c2e10183d81315fb41b96
Christopher Lo <christopherlo@berkeley.edu>
2012-11-30 01:41:21 -0800

    fixed seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 class User < ActiveRecord::Base
 PHONE_NUMBER_LENGTH_OF_COUNTRY = 12
-before_create do
-    self.phone_number = sanitize phone_number if attribute_present?("phone_number")
-end
-before_save do
-    self.phone_number = sanitize phone_number if attribute_present?("phone_number")
-end
-before_update do
+before_validation do
     self.phone_number = sanitize phone_number if attribute_present?("phone_number")
 end
 attr_accessible :location, :name, :phone_number
