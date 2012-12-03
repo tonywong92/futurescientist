c807980294a3f6fb8f787aab63bd0005f85310e2
Christopher Lo <christopherlo@berkeley.edu>
2012-12-03 00:40:40 -0800

    quick bug fix

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,10 @@
 class Skill < ActiveRecord::Base
  before_create do
-    self.password = skill_name.strip.downcase if attribute_present?("skill_name")
+    self.skill_name = skill_name.strip.downcase if attribute_present?("skill_name")
  end
 
   before_update do
-    self.password = skill_name.strip.downcase if attribute_present?("skill_name")
+    self.skill_name = skill_name.strip.downcase if attribute_present?("skill_name")
   end
 
   attr_accessible :skill_name
