5d5faf71e9b99433fc0888c94ea01477cc530f0e
James Won <james.won@berkeley.edu>
2012-12-05 02:23:07 -0800

    changed accounts migration to remove accepted_problems field

--- db/migrate/20121019221207_create_accounts.rb
+++ db/migrate/20121019221207_create_accounts.rb
@@ -7,7 +7,6 @@ class CreateAccounts < ActiveRecord::Migration
       t.string :email
       t.text :skills
       t.text :verified_skills
-      t.text :accepted_problems
       t.boolean :verified, :default => false
       t.references 'user'
 
